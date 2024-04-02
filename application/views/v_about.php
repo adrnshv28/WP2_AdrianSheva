0548dbae9e1880e00090b0d56b8a3eabb59a6bcd
adrnshv28 <adriansheva99@gmail.com>
2024-04-02 15:24:12 +0700

    Tugas Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()
+        ?>assets/stylebuku.css">
+</head>
+
+<body>
+    <section>
+        <h1>
+            <?php echo $judul ?>
+        </h1>
+        <h4>Nama</h4>
+        <ul type="disc">
+            <li>Nama Depan : Adrian</li>
+            <li>Nama Belakang : Sheva</li>
+        </ul>
+        <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+            <li> Jalan Mandor Demong No. 46 Babakan</li>
+        </ul>
+
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+            <li>Madinah</li>
+        </ul>
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+            <li>Rebahan</li>
+        </ul>
+    </section>
+</body>
+
+</html>
